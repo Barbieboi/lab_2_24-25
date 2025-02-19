cdb5c4e722479ef9213473d83a990c460269aef7
Enrico Barbin <e.barbin@studenti.unipi.it>
2025-02-19 02:40:11 +0100

    link.py completato

--- exercises/serpente/link.py
+++ exercises/serpente/link.py
@@ -6,34 +6,28 @@ import sys , os , os.path
 
 def link_directory(src, dest):
 	assert os.path.exists(src) and os.path.exists(dest)
-	aggiunti = {}
+	aggiunti = []
 	for base , directories , files in os.walk(src):
-		print(files)
 		for file in files: 
-			sourcefile = os.path.join(base, file)
-			if not os.access(sourcefile, os.F_OK):
+			sourcefile = os.path.abspath(os.path.join(base, file))
+			if not os.access(sourcefile, os.F_OK) or os.path.islink(sourcefile):
 				continue
 			link_dir = os.path.join(dest, (file[0].lower()))
 			if not os.path.exists(link_dir):
 				os.mkdir(link_dir)
-			
-			if file not in aggiunti:
-				aggiunti[file] = 1
-				link = str(file)
-			else :
-				link = str(file) + "." + str(aggiunti[file])
-				aggiunti[file] += 1
-
-			link_name = os.path.join(link_dir, file)
-			print(f"linkando {sourcefile} a {link_name}")
+			real_link = os.path.realpath(link_dir)
+			print(aggiunti)
+			link = f"{file}"
+			if file in aggiunti:
+				link = f"{file}.{aggiunti.count(file)}"
+			aggiunti.append(file)
+			link_name = os.path.join(real_link, link)
 			os.link(sourcefile, link_name)
 		
 
 
-
-
-
 def main(src, dest):
+	
 	if os.path.exists(dest):
 		sys.exit(1)
 	os.mkdir(dest)
